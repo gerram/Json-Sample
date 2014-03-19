95b62337c1ecbed440e6645b663ea0b3617b3035
Rajesh <Rajesh@Rajeshs-Macbook.local>
2014-03-19 10:32:55 +0530

    Initial Commit

---
+++ Json Sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Json Sample
+//
+//  Created by Rajesh on 19/03/14.
+//  Copyright (c) 2014 Rajesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Json Sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Json Sample
+//
+//  Created by Rajesh on 19/03/14.
+//  Copyright (c) 2014 Rajesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
